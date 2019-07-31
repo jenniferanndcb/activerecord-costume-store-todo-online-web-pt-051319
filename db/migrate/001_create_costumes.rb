93eb081670f7dbc1606ff9d17b06357da5f9a24a
Jen <jenniferanndcb@gmail.com>
2019-07-31 13:36:34 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -10,5 +10,14 @@
 
 class CreateCostumes < ActiveRecord::Migration[5.1]
   
-  
+  def change 
+    create_table :costumes do 
+      |t|
+      
+      t.string :name 
+      t.string :image_url 
+      t.string :size 
+    end  
+    
+  end 
 end 
