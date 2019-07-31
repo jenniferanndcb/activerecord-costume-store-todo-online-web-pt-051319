52419ca6040c5cc42688fc410d23f18a6d6c471f
Jen <jenniferanndcb@gmail.com>
2019-07-31 13:41:55 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -9,6 +9,8 @@ class CostumeStores < ActiveRecord::Migration[5.1]
       t.integer :number_of_costumes
       t.integer :number_of_employees 
       t.string :in_business?
-      t.
+      t.datetime :opening_time 
+      t.datetime :closing_time
+    end 
   end 
 end 
